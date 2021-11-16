0913696a235952b06193a60e3463ae3799b5c675
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-11-16 18:49:39 +0100

    Add win32 window

--- Game/Src/Source.cpp
+++ Game/Src/Source.cpp
@@ -1,6 +1,72 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
 #include <iostream>
+#include <windows.h>
+#include <wrl.h>
+
+
+const wchar_t wndClassName[] = L"wcName";
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+bool Win32MsgPump()
+{
+	MSG msg{};
+	while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+		if (msg.message == WM_QUIT)
+		{
+			std::cout << "WM_QUIT" << std::endl;
+			return false;
+		}
+	}
+	return true;
+}
 
 int main()
 {
+	HINSTANCE hInst = GetModuleHandle(nullptr);
+
+	WNDCLASSEX wc{};
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.hInstance = hInst;
+	wc.lpszClassName = wndClassName;
+	wc.lpfnWndProc = WindowProc;
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindowEx(0, wndClassName, L"WindowText",
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		nullptr, nullptr, hInst, nullptr); //last pointer is to some optional data of any kind that kan be usefull when getting messages
+
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+
+	while (Win32MsgPump())
+	{
+
+	}
+
 	return 0;
+}
+
+LRESULT WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+	{
+		std::cout << "WM_CLOSE" << std::endl;
+		DestroyWindow(hwnd);
+		return 0;
+	}
+	case WM_DESTROY:
+	{
+		std::cout << "WM_DESTROY" << std::endl;
+		PostQuitMessage(0);
+		return 0;
+	}
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }

af1e717fcaaf540f399da6560bf2554b08ed582b
Gunnar Cerne <bjorngunnar99@gmail.com>
2022-01-02 15:30:22 +0100

    test noise

--- TestProject/Src/Source.cpp
+++ TestProject/Src/Source.cpp
@@ -3,10 +3,41 @@
 #include <string>
 #include <vector>
 
+#pragma warning(push, 0)
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#pragma warning(pop)
+
 #include "PerlinNoise.hpp"
 
 int main()
 {
-	std::cout << "hej "<< std::endl;
+	constexpr uint32_t width = 1024;
+	constexpr uint32_t height = 1024;
+
+	const siv::PerlinNoise::seed_type seed = 123456u;
+
+	const siv::PerlinNoise perlin{ seed };
+	unsigned char* noise = new unsigned char[width * height]();
+	for (int y = 0; y < height; ++y)
+	{
+		for (int x = 0; x < width; ++x)
+		{
+			noise[y * width + x] = static_cast<unsigned char>(255 * perlin.octave2D_01((x * 0.01), (y * 0.01), 8, 0.8));
+		}
+	}
+	
+	if (!stbi_write_bmp("testNoise.bmp", width, height, STBI_grey, noise))
+	{
+		std::cout << "write error" << std::endl;
+	}
+	if (!stbi_write_png("testNoise.png", width, height, STBI_grey, noise, width * STBI_grey))
+	{
+		std::cout << "write error" << std::endl;
+	}
+
+	delete[] noise;
 	return 0;
 }

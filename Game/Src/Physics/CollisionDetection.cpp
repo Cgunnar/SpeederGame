369d6961802a4ac3425f5ad6832c833b18b58337
Gunnar Cerne <bjorngunnar99@gmail.com>
2022-01-10 18:10:55 +0100

    save

---
+++ Game/Src/Physics/CollisionDetection.cpp
@@ -0,0 +1,17 @@
+#include "pch.hpp"
+#include "CollisionDetection.h"
+#include "RfextendedMath.hpp"
+
+using namespace colDetect;
+using namespace rfm;
+
+std::vector<CollisionPoint> colDetect::PlaneVSPoints(Plane plane, std::vector<rfm::Vector3> points)
+{
+    for (const auto& p : points)
+    {
+        Vec
+    }
+
+
+    return std::vector<CollisionPoint>();
+}
--- Game/Src/Physics/CollisionDetection.h
+++ Game/Src/Physics/CollisionDetection.h
@@ -1,6 +1,17 @@
 #pragma once
-
 #include "RimfrostMath.hpp"
 #include "boundingVolumes.h"
 
+struct CollisionPoint
+{
+	rfm::Vector3 point;
+	float penetration;
+	rfm::Vector3 normal;
+};
+
+namespace colDetect
+{
+	std::vector<CollisionPoint> PlaneVSPoints(Plane plane, std::vector<rfm::Vector3> points);
+
+}
 
